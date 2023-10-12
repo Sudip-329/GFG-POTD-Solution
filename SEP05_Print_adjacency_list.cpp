6aeccad90e9c551546e32ad9fbbbf64920bfa9f0
Sudip <sudipchakrabarty6@gmail.com>
2023-10-12 05:56:05 +0530

    05-SEP-2023

---
+++ SEP05_Print_adjacency_list.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to return the adjacency list for each vertex.
+    vector<vector<int>> printGraph(int V, vector<pair<int,int>>edges) {
+        vector<vector<int>> g(V);  // here  g will have 4 pairs as v=4 for testcase 2
+        
+        for(auto i : edges){
+            g[i.first].push_back(i.second);  //g[i.first] means pair positon and
+            g[i.second].push_back(i.first);  // i.first menas the vertices 
+        }
+        
+        return g;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >> E;
+        vector<pair<int,int>>edges;
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            edges.push_back({u,v});
+        }
+        Solution obj;
+        vector<vector<int>> adj = obj.printGraph(V, edges);
+        for(int i=0;i<V;i++)
+        {
+            sort(adj[i].begin(),adj[i].end());
+            for(auto it:adj[i])
+                cout<<it<<" ";
+            cout<<endl;
+        }
+    }
+    return 0;
+}
+// } Driver Code Ends
