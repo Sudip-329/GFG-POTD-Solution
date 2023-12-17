1c622e41440d7dc3eaec8f2f233a84da0188c4a8
Sudip <sudipchakrabarty6@gmail.com>
2023-12-17 13:21:04 +0600

    17-DEC-2023

---
+++ DEC17_Max_Sum_without_Adjacents.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	// calculate the maximum sum with out adjacent
+	int findMaxSum(int *arr, int n) {
+	    // code here
+	    if(n==1) return arr[0];
+	    for(int i=2; i<n; i++)
+	    {
+	        arr[i] = arr[i] + arr[i-2];
+	        arr[i-1] = max(arr[i-2],arr[i-1]);
+	    }
+	    return max(arr[n-1],arr[n-2]);
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.findMaxSum(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
